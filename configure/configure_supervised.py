0eb93b0426cc67664fef0fca03defc2a3840e62b
ACHRAF EL OUAHABI <141170161+achrafelouahabi@users.noreply.github.com>
2025-12-24 14:34:16 +0100

    Update configure_supervised.py

--- configure/configure_supervised.py
+++ configure/configure_supervised.py
@@ -208,7 +208,7 @@ def get_default_config(data_name):
                 lr=4.0e-4,
                 pretrain_epochs=150,
                 batch_size=200,
-                epoch=500,
+                epoch=1000,
                 alpha=10,
                 lambda2=0.1,
                 lambda1=0.11,
